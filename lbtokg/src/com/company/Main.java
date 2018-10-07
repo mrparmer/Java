24cc2ad9f1ffe54ca834404308ebc1df99ca50aa
Matthew Parmer <37225891+mrparmer@users.noreply.github.com>
2018-10-06 22:44:16 -0400

    First night

---
+++ lbtokg/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double lbs = 200d;
+        double kgs = 0.45359237d;
+        double convert = lbs * kgs;
+
+        System.out.println(convert + " Kilograms");
+        //90.718474 kgs
+
+    }
+}
